4ed79fec2f4f46e45e93f67d161982c8cad7bd64
lunoxx <a.mese99@gmail.com>
2023-03-30 18:53:13 +0300

    Project upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './Assets/App.scss';
+
+// import { BallTriangle } from  'react-loader-spinner'
+// <BallTriangle
+//   height={100}
+//   width={100}
+//   radius={5}
+//   color="#fff"
+//   ariaLabel="ball-triangle-loading"
+//   wrapperClass={{}}
+//   wrapperStyle=""
+//   visible={true}
+// />
+
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+  // Loading screen
+  const [isLoading, setLoading] = useState(true);
+  
+  function onPageLoad() {
+    const loaderElement = document.querySelector(".loader-container");
+    if (loaderElement) {
+      loaderElement.remove();
+      setLoading(!isLoading);
+    }
+  }
+  
+  useEffect(() => {
+
+    if (document.readyState != "complete") {
+
+      window.addEventListener('load', onPageLoad);
+      return () => window.removeEventListener('load', onPageLoad);
+    }
+
+  });
+
+  if (isLoading) { //
+    return null;
+  }
+
+  // after content is loaded
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="wrapper">
+
+      <div className="mx-auto max-w-screen-xl px-3 py-6 text-white text-center h-full flex items-center justify-center">
+
+        <div className="text-container">
+
+          <div className='font-semibold uppercase text-xl'>One year of free subscription to 👇</div>
+          <div className="hero-logo text-6xl md:text-9xl my-5">NETFLIX</div>
+          <p className='text-lg font-semibold px-0 md:px-20 lg:px-80'>You only need a valid email address and a US residential address. The offer is available only for this country, at the moment.</p>
+
+          <div className='mt-14 font-semibold'>
+            <p>Use the button below to continue to the registration page.</p>
+
+            {/* Button */}
+            <div className='mt-8 mb-1'>
+
+              <form role="search" method="post" action="https://smrturl.co/o/477275/53321394?s1=" target="_blank">
+
+                <button className="bg-transparent text-xl uppercase text-white/90 hover:bg-slate-100 font-normal hover:text-gray-600 py-4 px-6 border border-red-500 hover:border-transparent rounded cursor-pointer"
+                  href = "https://smrturl.co/o/477275/53321394?s1="
+                  target="_blank"
+                  type="submit">
+
+                    Click here to continue
+                </button>
+              </form>
+            </div>
+          </div>
+
+        </div>
+        
+      </div>
+
     </div>
+
   );
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/*.{js,jsx,ts,tsx}",
+    "./src/Layouts/*.{js,jsx,ts,tsx}",
+    "./src/Components/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
